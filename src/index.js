9506642ffff0ae31128968cd9e54a219ac4d7dbb
Yuliya Zozulya <djulia@ukr.net>
2023-05-04 21:53:58 +0300

    adds files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+
+
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  <BrowserRouter basename="/easy-start-in-www">
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
+);
